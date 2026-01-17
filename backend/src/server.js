db856c11a5521f5f6715e3da22b267a552ba2635
pranavgudagi <pranavgudagi@gmail.com>
2026-01-17 23:32:04 +0530

    importing functions

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,14 +3,15 @@ import { ENV } from "./lib/env.js"
 import path from "path"
 import { connectDB } from "./lib/db.js"
 import cors from "cors";
-import {serve} from "ingest/express"
+import {serve} from "inngest/express"
+import { inngest,functions} from "./lib/inngest.js"
 const app=express();
 
 const __dirname=path.resolve()
 
 //middleware
 app.use(express.json())
-app.use(cors({origin:ENV.CLIENT_URL},credentials=true))
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
 
 app.use("/api/inngest",serve({client:inngest,functions}))
 
