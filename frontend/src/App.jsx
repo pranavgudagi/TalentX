e5c55a851f56c19015bc4103f48d1dd33de14a5d
pranavgudagi <pranavgudagi@gmail.com>
2026-01-19 16:54:22 +0530

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
+import { useState } from 'react'
+
+import viteLogo from '/vite.svg'
 import './App.css'
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react'
+import { SignedIn, SignedOut, SignInButton, UserButton, SignOutButton } from '@clerk/clerk-react'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
-     <h1>Welcome to TalentIQ</h1>
+     <h1>Welcome to the app</h1>
      <SignedOut>
       <SignInButton mode="modal">
-        <button>Sign In</button>
+        <button>Login</button>
       </SignInButton>
      </SignedOut>
 
